90f43012a677fe07bc74b27ca0b872e7624e57a5
asdf99245 <39851220+asdf99245@users.noreply.github.com>
2022-06-21 14:31:26 +0900

    Time: 96 ms (42.42%), Space: 44.9 MB (8.89%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.js
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.js
@@ -11,16 +11,16 @@
  * @return {number}
  */
 const sumOfLeftLeaves = function(root) {
-    if(!root) {
+    if(!root) { // 노드가 없으면 종료
         return 0;
     }
     
     let sum = 0;
-    if(root.left) {
-        if(!root.left.left && !root.left.right) sum += root.left.val;
+    if(root.left) { // left child가 존재할 시
+        if(!root.left.left && !root.left.right) sum += root.left.val; // left child가 leaf node이면
         else sum+= sumOfLeftLeaves(root.left);
     }
-    if(root.right) sum += sumOfLeftLeaves(root.right);
+    if(root.right) sum += sumOfLeftLeaves(root.right); // right child가 존재할 시
     
     return sum;
 };
