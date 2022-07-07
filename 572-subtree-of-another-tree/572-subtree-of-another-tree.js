2c4dd15ec82046cb74863b7ad73d5794543438a4
asdf99245 <39851220+asdf99245@users.noreply.github.com>
2022-07-07 13:00:26 +0900

    Time: 135 ms (37.21%), Space: 48.9 MB (89.11%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.js
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -12,24 +12,25 @@
  * @return {boolean}
  */
 const isEqual = (root, subRoot) => {
-    if(!subRoot && !root) return true;
-    else if((!subRoot && root) || (subRoot && !root)) return false;
+    if(!subRoot && !root) return true; // 더 이상 비교할 노드가 없을 때
+    else if((!subRoot && root) || (subRoot && !root)) return false; // 한쪽이 하위 노드가 더 많다면 실패
     
-    if(root.val !== subRoot.val) return false;
+    if(root.val !== subRoot.val) return false; // root와 subRoot의 value가 같지 않다면 실패
     
+    // left subtree와 right subtree가 모두 같아야 한다.
     return isEqual(root.left, subRoot.left) && isEqual(root.right, subRoot.right);
 }
 
 const isSubtree = function(root, subRoot) {
-    if(!root) return false;
+    if(!root) return false; 
     
     let ret;
     
-    if(root.val === subRoot.val){
+    if(root.val === subRoot.val){ // root value가 같다면 포함여부 검사
         ret = isEqual(root, subRoot);
     }
     
-    ret |= isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    ret |= isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot); // subtree가 포함되는 부분이 존재한다면 성공
     
     return ret;
 };
